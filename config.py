547c234be5d218e1c6f3100ea2bc9892148ec7a9
NeonAn <justasneon@gmail.com>
2025-11-21 11:12:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 NOTIFICATION_CHAT_ID = int(os.getenv("NOTIFICATION_CHAT_ID", 0))
 
 # Check interval in seconds (Default: 5 minutes)
-CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", 30))
+CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", 20))
 
 # Server Port (Required for cloud deployments)
 PORT = int(os.getenv("PORT", 8080))
