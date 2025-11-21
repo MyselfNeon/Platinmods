2f67715485a71ecbad222abe86e2d76184aa274a
NeonAnッ <justasneon@gmail.com>
2025-11-21 20:51:51 +0530

    Update __init__.py

--- MyselfNeon/__init__.py
+++ MyselfNeon/__init__.py
@@ -1,4 +1,1 @@
-# Expose submodules for Pyrogram command registration
-# Importing these files ensures their command handlers (@Client.on_message) are registered.
-from . import broadcast
-from . import checks
+
